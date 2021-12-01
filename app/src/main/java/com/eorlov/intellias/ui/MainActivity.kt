ac2b4235f99485032b8e0fab8405aca3190cc05c
Eugene <evgeniiorlov204@gmail.com>
2021-12-01 21:02:27 +0200

    Improved app's architecture

--- app/src/main/java/com/eorlov/intellias/ui/MainActivity.kt
+++ app/src/main/java/com/eorlov/intellias/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.eorlov.intellias
+package com.eorlov.intellias.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eorlov.intellias.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
