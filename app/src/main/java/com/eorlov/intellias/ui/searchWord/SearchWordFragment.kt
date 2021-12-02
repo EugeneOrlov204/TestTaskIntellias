c28e10c5581aadc8c11b5e5809c07b3c8c84699a
Eugene <evgeniiorlov204@gmail.com>
2021-12-02 09:32:07 +0200

    Implemented fragments and its view models

---
+++ app/src/main/java/com/eorlov/intellias/ui/searchWord/SearchWordFragment.kt
@@ -0,0 +1,10 @@
+package com.eorlov.intellias.ui.searchWord
+
+import com.eorlov.intellias.base.BaseFragment
+import com.eorlov.intellias.databinding.FragmentSearchWordBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchWordFragment: BaseFragment<FragmentSearchWordBinding>(FragmentSearchWordBinding::inflate) {
+
+}
---
+++ app/src/main/java/com/eorlov/intellias/ui/settings/SettingsFragment.kt
@@ -0,0 +1,9 @@
+package com.eorlov.intellias.ui.settings
+
+import com.eorlov.intellias.base.BaseFragment
+import com.eorlov.intellias.databinding.FragmentSettingsBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SettingsFragment : BaseFragment<FragmentSettingsBinding>(FragmentSettingsBinding::inflate) {
+}
